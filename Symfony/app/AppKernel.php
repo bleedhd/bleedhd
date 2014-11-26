b14a4212d3c15a78f5c15ad93a75569a841475c5
Lukas Angerer <lukas.angerer@getunik.com>
2014-11-26 13:20:02 +0100

    POMCANBLUT-79: added (almost) empty data bundle

--- Symfony/app/AppKernel.php
+++ Symfony/app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Getunik\BleedHd\SecurityBundle\GetunikBleedHdSecurityBundle(),
+            new Getunik\BleedHd\AssessmentDataBundle\GetunikBleedHdAssessmentDataBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ Symfony/src/Getunik/BleedHd/AssessmentDataBundle/GetunikBleedHdAssessmentDataBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Getunik\BleedHd\AssessmentDataBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GetunikBleedHdAssessmentDataBundle extends Bundle
+{
+}
