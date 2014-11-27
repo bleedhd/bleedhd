fe0a9acfcee848ab22029bf00baf5565259accef
Lukas Angerer <lukas.angerer@getunik.com>
2014-11-27 13:54:24 +0100

    POMCANBLUT-80: finalized basic OAuth mechanism and configuration

--- Symfony/src/Getunik/BleedHd/SecurityBundle/Controller/DefaultController.php
+++ Symfony/src/Getunik/BleedHd/SecurityBundle/Controller/DefaultController.php
@@ -3,12 +3,33 @@
 namespace Getunik\BleedHd\SecurityBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function getTokenAction(Request $request)
     {
-    	$env = $this->container->get('kernel')->getEnvironment();
-        return $this->render('GetunikBleedHdSecurityBundle:Default:index.html.twig', array('name' => $name, 'env' => $env));
+        $user = $this->getUser();
+        $session = $request->getSession();
+        $token = $session->get('getunik_bleed_hd_security.oauth_token');
+
+        return new JsonResponse($token);
+    }
+
+    public function refreshTokenAction()
+    {
+        $request = Request::create(
+            '/getToken',
+            'GET'
+        );
+
+        $kernel = $this->container->get('kernel');
+
+        $response = $kernel->handle($request, HttpKernelInterface::SUB_REQUEST, false);
+
+        return new JsonResponse(array('fib' => array(1, 1, 2, 3, 5, 8, 13), 'res' => $response->getContent()));
     }
 }
--- Symfony/src/Getunik/BleedHd/SecurityBundle/Entity/Client.php
+++ Symfony/src/Getunik/BleedHd/SecurityBundle/Entity/Client.php
@@ -3,52 +3,17 @@
 namespace Getunik\BleedHd\SecurityBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use FOS\OAuthServerBundle\Model\Client as BaseClient;
 
 /**
  * Client
  */
-class Client
+class Client extends BaseClient
 {
-    /**
-     * @var integer
-     */
-    private $id;
-
     /**
      * @var string
      */
     private $name;
-
-    /**
-     * @var string
-     */
-    private $randomId;
-
-    /**
-     * @var array
-     */
-    private $redirectUris;
-
-    /**
-     * @var string
-     */
-    private $secret;
-
-    /**
-     * @var array
-     */
-    private $allowedGrantTypes;
-
-
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
 
     /**
      * Set name
@@ -66,102 +31,10 @@ class Client
     /**
      * Get name
      *
-     * @return string 
+     * @return string
      */
     public function getName()
     {
         return $this->name;
-    }
-
-    /**
-     * Set randomId
-     *
-     * @param string $randomId
-     * @return Client
-     */
-    public function setRandomId($randomId)
-    {
-        $this->randomId = $randomId;
-
-        return $this;
-    }
-
-    /**
-     * Get randomId
-     *
-     * @return string 
-     */
-    public function getRandomId()
-    {
-        return $this->randomId;
-    }
-
-    /**
-     * Set redirectUris
-     *
-     * @param array $redirectUris
-     * @return Client
-     */
-    public function setRedirectUris($redirectUris)
-    {
-        $this->redirectUris = $redirectUris;
-
-        return $this;
-    }
-
-    /**
-     * Get redirectUris
-     *
-     * @return array 
-     */
-    public function getRedirectUris()
-    {
-        return $this->redirectUris;
-    }
-
-    /**
-     * Set secret
-     *
-     * @param string $secret
-     * @return Client
-     */
-    public function setSecret($secret)
-    {
-        $this->secret = $secret;
-
-        return $this;
-    }
-
-    /**
-     * Get secret
-     *
-     * @return string 
-     */
-    public function getSecret()
-    {
-        return $this->secret;
-    }
-
-    /**
-     * Set allowedGrantTypes
-     *
-     * @param array $allowedGrantTypes
-     * @return Client
-     */
-    public function setAllowedGrantTypes($allowedGrantTypes)
-    {
-        $this->allowedGrantTypes = $allowedGrantTypes;
-
-        return $this;
-    }
-
-    /**
-     * Get allowedGrantTypes
-     *
-     * @return array 
-     */
-    public function getAllowedGrantTypes()
-    {
-        return $this->allowedGrantTypes;
     }
 }
