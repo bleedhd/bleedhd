4e4812bf771a1004a846927e7091f240f38f50fa
Lukas Angerer <lukas.angerer@getunik.com>
2015-01-19 14:48:25 +0100

    BBSBLEED-46: setup of new text question type

---
+++ Symfony/src/Getunik/BleedHd/AssessmentUIBundle/Resources/app/components/question/question-types/text.type.js
@@ -0,0 +1,22 @@
+
+(function (angular) {
+
+	angular.module('question').config(function (QuestionTypeRegistryProvider) {
+		QuestionTypeRegistryProvider.registerTypeWithName('text', 'base', function (parent) {
+			return {
+				ctor: function TextQuestion(scope, question) {
+					parent(this)(scope, question);
+				},
+				members: {
+					link: function (element) {
+					},
+					onChange: function (option) {
+						//this.data.supplements = option.supplementData;
+						//this.scope.$emit('q-data-changed', this);
+					},
+				},
+			};
+		});
+	});
+
+})(angular);
