18e972c4978e916c15115eb8528a2b785528fb84
Lukas Angerer <lukas.angerer@getunik.com>
2015-01-20 08:50:17 +0100

    BBSBLEED-46: implemented binding filter allow only certain input

--- Symfony/src/Getunik/BleedHd/AssessmentUIBundle/Resources/app/components/question/question-types/text.type.js
+++ Symfony/src/Getunik/BleedHd/AssessmentUIBundle/Resources/app/components/question/question-types/text.type.js
@@ -1,11 +1,27 @@
 
 (function (angular) {
 
+	var regexExp = /^\/(.*)\/(.*)$/,
+		namedExpressions = {
+			integer: /^\d*$/,
+			decimal: /^\d*(\.\d*)?$/,
+		};
+
 	angular.module('question').config(function (QuestionTypeRegistryProvider) {
 		QuestionTypeRegistryProvider.registerTypeWithName('text', 'base', function (parent) {
 			return {
 				ctor: function TextQuestion(scope, question) {
 					parent(this)(scope, question);
+
+					this.filterExpression = null;
+					if (question.pattern !== undefined) {
+						var match = question.pattern.match(regexExp);
+						if (match !== null) {
+							this.filterExpression = new RegExp(match[1], match[2]);
+						} else {
+							this.filterExpression = namedExpressions[question.pattern];
+						}
+					}
 				},
 				members: {
 					link: function (element) {
