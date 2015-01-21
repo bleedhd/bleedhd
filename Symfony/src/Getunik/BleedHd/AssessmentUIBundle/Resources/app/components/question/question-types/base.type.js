5ec9073d5bc1e150077061493230aca91a0fbc22
Lukas Angerer <lukas.angerer@getunik.com>
2015-01-21 15:45:52 +0100

    BBSBLEED-26: code comments for base question and supplement

--- Symfony/src/Getunik/BleedHd/AssessmentUIBundle/Resources/app/components/question/question-types/base.type.js
+++ Symfony/src/Getunik/BleedHd/AssessmentUIBundle/Resources/app/components/question/question-types/base.type.js
@@ -13,21 +13,39 @@
 					this.data.supplements = this.normalizeSupplement(this.data.supplements);
 				},
 				members: {
+					/**
+					 * @returns {Array} - an array of potential fallback template names to load with this question type
+					 */
 					getTemplateHierarchy: function () {
 						return [
 							this.definition.type + '-' + this.definition.variant,
 							this.definition.type,
 						];
 					},
+					/**
+					 * @returns {Object} - this question type's null data object with a 'value' and a 'supplements' property
+					 */
 					emptyData: function () {
 						return {
 							value: null,
 							supplements: {},
 						};
 					},
+					/**
+					 * Resets the question to its default empty state
+					 *
+					 * @param {Object} event - the AngularJS event that triggered the reset
+					 * @param {Object} data - the AngularJS event data from the triggering event
+					 */
 					reset: function (event, data) {
 						this.data = this.emptyData();
 					},
+					/**
+					 * Performs AngularJS style 'linking' of the question's compiled template with the
+					 * question controller (which is the current question type instance)
+					 *
+					 * @param {jQueryObj} element - the question directive's root element which contains all the compiled templates at this stage
+					 */
 					link: function (element) {
 						var that = this;
 
@@ -46,9 +64,21 @@
 							that.scope.$emit('q-data-changed', that);
 						});
 					},
+					/**
+					 * ng-change event callback that is called whenever the result of this question changes.
+					 * This simply triggers the 'q-data-changed' event to notify its parent container.
+					 */
 					onChange: function () {
 						this.scope.$emit('q-data-changed', this);
 					},
+					/**
+					 * Due to a funny JavaScript / PHP semantic gap, the supplements 'objects' that are retrieved
+					 * from the server actually arrive as arrays if the object was empty when it was saved...
+					 * This function normalizes the supplements back into their intended form.
+					 *
+					 * @param {Object|Array} supplements - the supplements object that should be normalized
+					 * @returns {Object} the given supplements object if it was already an object, otherwise it returns an empty object
+					 */
 					normalizeSupplement: function (supplements) {
 						// the fun of translating objects to JSON, to PHP array, to JSON (in the DB), back to PHP array,
 						// back to JSON, back to objects... {} => JSON {} => PHP array() => JSON [] => PHP array() => JSON [] => []
