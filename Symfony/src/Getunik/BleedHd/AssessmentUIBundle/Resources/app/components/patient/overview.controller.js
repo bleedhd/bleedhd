c1586bd7f4c1958e50d1f7aae4b1bb5944ed80bc
Lukas Angerer <lukas.angerer@getunik.com>
2015-01-26 12:35:55 +0100

    BBSBLEED-29: patients overview list paging

--- Symfony/src/Getunik/BleedHd/AssessmentUIBundle/Resources/app/components/patient/overview.controller.js
+++ Symfony/src/Getunik/BleedHd/AssessmentUIBundle/Resources/app/components/patient/overview.controller.js
@@ -3,27 +3,66 @@
 
 	function PatientOverviewController($scope, $filter, patients) {
 		this.patients = patients;
+		this.patients.sort(function (a, b) { return a.lastname > b.lastname; });
+
+		this.paging = {
+			items: [],
+			currentPage: 1,
+			itemsPerPage: 5,
+			getPageItems: function () {
+				var start = (this.currentPage - 1) * this.itemsPerPage,
+					end = start + this.itemsPerPage;
+
+				return this.items.slice(start, end);
+			},
+		};
+		$scope.paging = this.paging;
+
 		this.birthdateFilter = $filter('birthdate');
 		this.resetFilter();
+
+		$scope.setPage = function (pageNo) {
+			$scope.paging.currentPage = pageNo;
+			console.log("on page", pageNo);
+		};
 	}
 
 	bleedHd.registerController('patient', PatientOverviewController,
 		{
-			filterPatientsFulltext: function (pattern) {
-				var ctl = this;
+			// filterPatientsFulltext: function (pattern) {
+			// 	var ctl = this;
 
-				// build a custom 'full text' search string from the patient properties and
-				// match it against a regular expression created from the given pattern value
-				return function (value, index) {
-					var fullText = [value.firstname, value.lastname, ctl.birthdateFilter(value.birthdate)].join('|'),
-						search = new RegExp(pattern, 'i');
+			// 	// build a custom 'full text' search string from the patient properties and
+			// 	// match it against a regular expression created from the given pattern value
+			// 	return function (value, index) {
+			// 		var fullText = [value.firstname, value.lastname, ctl.birthdateFilter(value.birthdate)].join('|'),
+			// 			search = new RegExp(pattern, 'i');
 
-					return fullText.match(search) !== null;
-				};
-			},
+			// 		return fullText.match(search) !== null;
+			// 	};
+			// },
 			resetFilter: function () {
 				this.filterActive = true;
 				this.filterSearch = '';
+				this.onFilterChange();
+			},
+			onFilterChange: function () {
+				this.paging.currentPage = 1;
+				this.paging.items = this.getPatientsPage();
+			},
+			getPatientsPage: function () {
+				var start = (this.paging.currentPage - 1) * this.paging.itemsPerPage,
+					end = start + this.paging.itemsPerPage;
+
+				return this.patients.filter(this.patientFilter.bind(this));
+			},
+			patientFilter: function (patient) {
+				if (this.filterActive && !patient.is_active) { return false; }
+
+				var fullText = [patient.firstname, patient.lastname, this.birthdateFilter(patient.birthdate)].join('|'),
+					search = new RegExp(this.filterSearch, 'i');
+
+				return fullText.match(search) !== null;
 			},
 		},
 		{
