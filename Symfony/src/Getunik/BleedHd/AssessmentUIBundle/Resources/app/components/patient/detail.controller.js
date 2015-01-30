13fec3f4d00557221087db2b8f9b6264f394495b
Lukas Angerer <lukas.angerer@getunik.com>
2015-01-30 10:10:06 +0100

    BBSBLEED-114: implemented assessment filter

--- Symfony/src/Getunik/BleedHd/AssessmentUIBundle/Resources/app/components/patient/detail.controller.js
+++ Symfony/src/Getunik/BleedHd/AssessmentUIBundle/Resources/app/components/patient/detail.controller.js
@@ -2,12 +2,40 @@
 (function (angular, bleedHd) {
 
 	function PatientDetailController($scope, PatientData, patient) {
+		this.$scope = $scope;
 		this.PatientData = PatientData;
 		this.patient = patient;
+
+		this.resetAssessmentFilter();
 	}
 
 	bleedHd.registerController('patient', PatientDetailController,
-		{},
+		{
+			byAssessmentType: function () {
+				var ctl = this;
+
+				return function (assessment, index) {
+					var type = ctl.getAssessmentType(assessment);
+					return ctl.assessmentFilter[type];
+				};
+			},
+			getAssessmentType: function(assessment) {
+				if (assessment.questionnaire === 'who') {
+					return 'who';
+				} else if (assessment.questionnaire === 'bsms') {
+					return 'bsms';
+				} else {
+					return 'gvhd';
+				}
+			},
+			resetAssessmentFilter: function () {
+				this.assessmentFilter = {
+					who: true,
+					bsms: true,
+					gvhd: true,
+				};
+			},
+		},
 		{
 			asName: 'ctlPatient',
 			templateUrl: bleedHd.getView('patient', 'detail'),
