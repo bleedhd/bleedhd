50d40c8943c2fb4f837fcb0bec04f18f7771d6f8
Lukas Angerer <lukas.angerer@getunik.com>
2015-02-12 11:21:10 +0100

    BBSBLEED-85: implemented basic token refresh

--- Symfony/src/Getunik/BleedHd/SecurityBundle/Controller/DefaultController.php
+++ Symfony/src/Getunik/BleedHd/SecurityBundle/Controller/DefaultController.php
@@ -16,20 +16,48 @@ class DefaultController extends Controller
         $session = $request->getSession();
         $token = $session->get('getunik_bleed_hd_security.oauth_token');
 
+        // refresh the session lifetime
+        $session->migrate();
+
         return new JsonResponse($token);
     }
 
-    public function refreshTokenAction()
+    public function refreshTokenAction(Request $request)
     {
-        $request = Request::create(
-            '/getToken',
-            'GET'
+        $clientId = $this->container->getParameter('getunik_bleed_hd_security.auto_token_client.id');
+        $clientSecret = $this->container->getParameter('getunik_bleed_hd_security.auto_token_client.secret');
+
+        $user = $this->getUser();
+        $session = $request->getSession();
+        $token = $session->get('getunik_bleed_hd_security.oauth_token');
+
+        if (empty($token)) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $refreshRequest = Request::create(
+            '/oauth/v2/token',
+            'GET',
+            array(
+                'client_id' => $clientId,
+                'client_secret' => $clientSecret,
+                'grant_type' => 'refresh_token',
+                'refresh_token' => $token->refresh_token,
+            )
         );
 
-        $kernel = $this->container->get('kernel');
+        $response = $this->get('kernel')->handle($refreshRequest, HttpKernelInterface::SUB_REQUEST, false);
+        $auth = json_decode($response->getContent());
 
-        $response = $kernel->handle($request, HttpKernelInterface::SUB_REQUEST, false);
+        // compute expiration date/time and add it to the token information
+        $now = new \DateTime();
+        $auth->expires_at = $now->add(new \DateInterval('PT' . 3600 . 'S'))->format(\DateTime::ISO8601);
+        $auth->uid = ($user instanceof User ? $user->getId() : -1);
 
-        return new JsonResponse(array('fib' => array(1, 1, 2, 3, 5, 8, 13), 'res' => $response->getContent()));
+        $session->set('getunik_bleed_hd_security.oauth_token', $auth);
+        // refresh the session lifetime
+        $session->migrate();
+
+        return new JsonResponse($auth);
     }
 }
