bca63272bad6828efcc5f6575fc536171e9ebff5
Lukas Angerer <lukas.angerer@getunik.com>
2015-02-23 13:31:45 +0100

    BBSBLEED-178: new field to store questionnaire version information

---
+++ Symfony/app/DoctrineMigrations/Version20150223132605.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150223132605 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Assessment ADD questionnaireVersion VARCHAR(255) DEFAULT NULL AFTER questionnaire');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Assessment DROP questionnaireVersion');
+    }
+}
