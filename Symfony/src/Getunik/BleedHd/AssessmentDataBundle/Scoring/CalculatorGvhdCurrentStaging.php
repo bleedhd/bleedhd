e01e9a0e462cd408ad1d56cfaea85a5faa6e6d8f
Lukas Angerer <lukas.angerer@getunik.com>
2015-02-25 16:00:55 +0100

    BBSBLEED-111: basic organ/category scoring

--- Symfony/src/Getunik/BleedHd/AssessmentDataBundle/Scoring/CalculatorGvhdCurrentStaging.php
+++ Symfony/src/Getunik/BleedHd/AssessmentDataBundle/Scoring/CalculatorGvhdCurrentStaging.php
@@ -12,6 +12,68 @@ class CalculatorGvhdCurrentStaging extends CalculatorBase
 	{
 		parent::__construct($logger);
 
-		// 'GvHD present', 'No GvHD present'
+		$this->score->category = new \stdClass();
+	}
+
+	protected function accumulateMapping(ScoreMapping $mapping)
+	{
+		$config = $mapping->getConfig();
+		$value = $mapping->getValue();
+
+		// counting of 'diagnostic' / 'distinctive' types
+		if (isset($config['category']))
+		{
+			if (!isset($this->score->category->{$config['category']}))
+			{
+				$this->score->category->{$config['category']} = array(
+					'value' => 0,
+					'override' => NULL,
+				);
+			}
+
+			$cat = &$this->score->category->{$config['category']};
+
+			if (isset($config['nongvhd']))
+			{
+				if ($config['nongvhd'] == $value)
+				{
+					$this->logger->info(" => overriding " . $config['category'] . " score: nongvhd");
+					$cat['override'] = 'nongvhd';
+				}
+			}
+			else
+			{
+				if (isset($config['value']))
+				{
+					$orig = $value;
+					$value = $this->getRangeValue($config['value'], $orig);
+					$this->logger->info(" => mapping range: " . $orig . " -> " . $value);
+				}
+
+				$this->logger->info(" => scoring: " . $config['category'] . " with " . $value);
+				$cat['value'] = max($cat['value'], $value);
+			}
+		}
+	}
+
+	protected function finish()
+	{
+		//$this->score->total = $this->getTotalScore();
+	}
+
+	protected function getRangeValue($ranges, $value)
+	{
+		foreach ($ranges as $segment)
+		{
+			$range = $segment['range'];
+			$result = $segment['value'];
+
+			if (count($range) === 0 || ($range[0] <= $value && $value < $range[1]))
+			{
+				return $result;
+			}
+		}
+
+		return NULL;
 	}
 }
