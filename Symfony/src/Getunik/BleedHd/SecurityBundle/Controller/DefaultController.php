0197ede09456f482c9a5b784c7875098c196a37b
Lukas Angerer <lukas.angerer@getunik.com>
2015-04-15 13:55:41 +0200

    BBSBLEED-211: added periodic server pings to prevent premature PHP session expiration

--- Symfony/src/Getunik/BleedHd/SecurityBundle/Controller/DefaultController.php
+++ Symfony/src/Getunik/BleedHd/SecurityBundle/Controller/DefaultController.php
@@ -43,4 +43,19 @@ class DefaultController extends Controller
             return $this->createAccessDeniedException();
         }
     }
+
+    /**
+     * The only purpose of this action is to keep the user's session alive...
+     * With PHP session handling, any session that has not been accessed for more
+     * than 1440 seconds (24 min; default value in php.ini) "may" be garbage collected.
+     * Since all REST requests don't actually access the session (as it should be),
+     * the sessions tend to expire after the 24 minutes unless we ping the server
+     * with a non-REST-ful request every now and then.
+     *
+     * See http://php.net/manual/en/session.configuration.php for more information
+     */
+    public function pingAction()
+    {
+        return new JsonResponse(array());
+    }
 }
