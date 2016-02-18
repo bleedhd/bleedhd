23c9d53bc22942228e2d4c728a0e092d41bf10c2
Lukas Angerer <lukas.angerer@getunik.com>
2016-02-18 16:14:05 +0100

    BBSBLEED-272: Using assessment context in edit controller instead of assessment only

--- Symfony/src/Getunik/BleedHd/AssessmentUIBundle/Resources/app/components/assessment/edit.controller.js
+++ Symfony/src/Getunik/BleedHd/AssessmentUIBundle/Resources/app/components/assessment/edit.controller.js
@@ -1,24 +1,24 @@
 
 (function (angular, bleedHd) {
 
-	function AssessmentEditController($location, $templateCache, BleedHdConfig, AssessmentData, HeaderControl, patient, assessment, group) {
+	function AssessmentEditController($location, $templateCache, BleedHdConfig, AssessmentData, HeaderControl, patient, context, group) {
 		HeaderControl.hide();
 
 		this.BleedHdConfig = BleedHdConfig;
 		this.AssessmentData = AssessmentData;
 		this.patient = patient;
-		this.assessment = assessment;
+		this.context = context;
 		this.$location = $location;
 		this.$templateCache = $templateCache;
 
-		this.secondaryScoreTemplate = this.getSubTemplate('secondary-score-' + this.assessment.questionnaire);
+		this.secondaryScoreTemplate = this.getSubTemplate('secondary-score-' + this.context.assessment.questionnaire);
 	}
 
 	bleedHd.registerController('assessment', AssessmentEditController,
 		{
 			delete: function () {
 				var ctl = this;
-				ctl.AssessmentData.deleteAssessment(ctl.assessment).then(function () {
+				ctl.AssessmentData.deleteAssessment(ctl.context.assessment).then(function () {
 					ctl.$location.path(['/patients', 'detail', ctl.patient.id].join('/'));
 				});
 			},
@@ -32,9 +32,9 @@
 			templateUrl: bleedHd.getView('assessment', 'edit'),
 			resolve: {
 				patient: function ($route, PatientData) { return PatientData.getPatient($route.current.params.patientId); },
-				assessment: function ($route, AssessmentData) {
+				context: function ($route, AssessmentContext) {
 					var params = $route.current.params;
-					return AssessmentData.getAssessmentFull(params.patientId, params.assessmentId);
+					return AssessmentContext(params.patientId, params.assessmentId);
 				},
 				group: function ($route) { return $route.current.params.group; },
 			},
