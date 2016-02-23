74e771189d80d84560d10a4bc58246c11886006a
Lukas Angerer <lukas.angerer@getunik.com>
2016-02-23 15:44:21 +0100

    BBSBLEED-288: Improved setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,16 +7,29 @@ ENV=${1:-prod}
 
 pushd "$SYMFONY"
 
+composer install
+
 # Configure var directory access
+echo "Configuring cache and log access using setfacl"
 HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
 sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
 sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
 
+echo "Initializing the database"
+$CONSOLE doctrine:migrations:migrate --env=$ENV -n
+
+echo "Setting up the application"
+$CONSOLE getu:oauth-server:create-client BleedHD --grant-type="password" --grant-type="refresh_token"
+
+echo "Please note/copy the two OAuth tokens above and then press RETURN to continue"
+read
+
+$CONSOLE fos:user:create --super-admin
 $CONSOLE assets:install --symlink --env=$ENV
 $CONSOLE assetic:dump --env=$ENV
-$CONSOLE doctrine:migrations:migrate --env=$ENV
-$CONSOLE getu:oauth-server:create-client BleedHD --grant-type="password" --grant-type="refresh_token"
-$CONSOLE fos:user:create --super-admin
 $CONSOLE cache:clear --env=$ENV
+
+echo ""
+echo "The setup is complete. You can now add the OAuth to the parameters.yml file"
 
 popd
