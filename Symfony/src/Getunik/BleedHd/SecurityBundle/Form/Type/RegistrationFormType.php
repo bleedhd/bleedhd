bf24f86cbc3111945f7f21e9bb3300b12f8b1c65
Lukas Angerer <lukas.angerer@getunik.com>
2016-04-04 14:56:50 +0200

    BBSBLEED-304: restored registration and profile pages to a working state

--- Symfony/src/Getunik/BleedHd/SecurityBundle/Form/Type/RegistrationFormType.php
+++ Symfony/src/Getunik/BleedHd/SecurityBundle/Form/Type/RegistrationFormType.php
@@ -4,6 +4,7 @@ namespace Getunik\BleedHd\SecurityBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 
 class RegistrationFormType extends AbstractType
@@ -11,23 +12,22 @@ class RegistrationFormType extends AbstractType
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
 		// add your custom field
-		$builder->add('roles', 'collection', array(
-			'type'   => 'choice',
-			'options'  => array(
-				'choices'  => array(
-					'ROLE_READER' => 'Reader',
-					'ROLE_ASSESSOR' => 'Assessor',
-					'ROLE_SUPERVISOR' => 'Supervisor',
-					'ROLE_ADMIN' => 'Admin',
-				),
-			)
+		$builder->add('roles', ChoiceType::class, array(
+			'choices_as_values' => true,
+			'multiple' => true,
+			'choices' => array(
+				'Reader' => 'ROLE_READER',
+				'Assessor' => 'ROLE_ASSESSOR',
+				'Supervisor' => 'ROLE_SUPERVISOR',
+				'Admin' => 'ROLE_ADMIN',
+			),
 		));
 		$builder->add('fullName');
 	}
 
 	public function getParent()
 	{
-		return 'fos_user_registration';
+		return 'FOS\UserBundle\Form\Type\RegistrationFormType';
 	}
 
 	public function getName()
