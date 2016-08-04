b622b678df505180eb021aa8bfdaafe801ca5371
Lukas Angerer <lukas.angerer@getunik.com>
2016-08-04 09:24:30 +0200

    BBSBLEED-104: bit vector transform implementation

---
+++ Symfony/src/Getunik/BleedHd/AssessmentDataBundle/Export/Transform/BitVector.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Getunik\BleedHd\AssessmentDataBundle\Export\Transform;
+
+
+use Getunik\BleedHd\AssessmentDataBundle\Export\ValueTypes\IDataValue;
+
+
+class BitVector extends BaseTransform
+{
+	private $trueValue;
+	private $falseValue;
+	private $nullValue;
+	private $separator;
+
+	public function __construct(array $config)
+	{
+		parent::__construct($config);
+
+		$this->trueValue = isset($config['trueValue']) ? $config['trueValue'] : '1';
+		$this->falseValue = isset($config['falseValue']) ? $config['falseValue'] : '0';
+		$this->nullValue = isset($config['nullValue']) ? $config['nullValue'] : '';
+		$this->separator = isset($config['separator']) ? $config['separator'] : '';
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function transform(IDataValue $raw)
+	{
+		$raw = self::requireActualResultValue($raw);
+		$valueArray = self::requireMultivalued($raw);
+
+		if ($valueArray === NULL) {
+			return $this->nullValue;
+		}
+
+		$question = $raw->getQuestionConfig();
+		$bits = [];
+
+		foreach ($question['options'] as $option) {
+			$bits[] = array_search($option['value'], $valueArray) !== false;
+		}
+
+		$result = implode($this->separator, array_map([$this, 'bitToString'], $bits));
+
+		return $result;
+	}
+
+	/**
+	 * @param $bit bool a single bit of the bit vector
+	 * @return mixed|string
+	 */
+	private function bitToString($bit)
+	{
+		return $bit ? $this->trueValue : $this->falseValue;
+	}
+}
